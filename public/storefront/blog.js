053f8c257721320f4e49f52fe7316cbb7d04c25e
shaofeiliu <2397667718@qq.com>
2026-09-19 21:46:13 +1200

    feat: bugfix

--- public/storefront/blog.js
+++ public/storefront/blog.js
@@ -7,8 +7,30 @@
 
     body.classList.add("bc-blog-enhanced");
 
+    // Cornerstone: each post is <article class="blog">.
+    var articles = Array.prototype.slice.call(
+      document.querySelectorAll("article.blog"),
+    );
+
+    if (articles.length) {
+      if (!document.querySelector(".bc-blog-list")) {
+        var list = document.createElement("div");
+        list.className = "bc-blog-list";
+        articles[0].parentNode.insertBefore(list, articles[0]);
+        articles.forEach(function (article, index) {
+          article.classList.add("bc-blog-card");
+          if (index === 0) {
+            article.classList.add("bc-blog-featured");
+          }
+          list.appendChild(article);
+        });
+      }
+      return;
+    }
+
+    // Dev mock / alternate markup: .blog > .blog-post
     var posts = document.querySelectorAll(
-      ".blog .blog-post, .blog-posts .blog-post",
+      ".blog > .blog-post, .blog-posts > .blog-post",
     );
     Array.prototype.forEach.call(posts, function (post, index) {
       post.classList.add("bc-blog-card");
